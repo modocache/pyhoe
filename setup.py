53edef3d00821224af82c08da8f8f891aafc2754
modocache <modocache@gmail.com>
2012-02-19 02:26:30 +0900

    Modified long description for PyPI.

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-import os
 from setuptools import setup, find_packages
-
-def get_long_description():
-    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
-        return f.read()
 
 setup(
     name = "pyhoe",
@@ -12,7 +7,25 @@ setup(
     author = 'modocache',
     author_email = 'modocache@gmail.com',
     description = 'A project skeleton generator.',
-    long_description = get_long_description(),
+    long_description = \
+    """
+    Like Ruby's `Hoe <https://github.com/seattlerb/hoe>`_, pyhoe aims
+    to help developers by providing a basic project skeleton.
+    Projects created with pyhoe's default settings include the following:
+
+     * A basic directory structure with templates for README, LICENSE,
+       `setup.py <http://docs.python.org/distutils/setupscript.html>`_,
+       `.gitignore <http://help.github.com/ignore-files/>`_, and other
+       ubiquitous project files.
+     * A `virtualenv <http://pypi.python.org/pypi/virtualenv>`_ for the new
+       project.
+     * Automated tests using `Nosy <https://bitbucket.org/douglatornell/nosy>`_,
+       which is built upon `nose <http://readthedocs.org/docs/nose/en/latest/>`_.
+       Mac OSX users will find a nose plugin called `Sneazr <https://github.com/jessemiller/Sneazr>`_
+       to display Growl notifications on tests results.
+     * `Tox <http://tox.readthedocs.org/en/latest/index.html>`_, for testing
+       across multiple Python environments.
+    """,
     keywords = "project setup skeleton",
     tests_require = ["nose"],
     install_requires = [],
@@ -20,7 +33,7 @@ setup(
     include_package_data = True,
     classifiers = [
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
         'Development Status :: 2 - Pre-Alpha',
         'Operating System :: MacOS',
         'Operating System :: Unix',
